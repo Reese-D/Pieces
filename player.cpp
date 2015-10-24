6e409b6e164d35934414b11f05ff07df1e889d10
Andrew Peterson <andrewpeterson@Andrews-MacBook-Pro.local>
2015-10-24 17:57:03 -0400

    finsihed

--- player.cpp
+++ player.cpp
@@ -124,24 +124,42 @@ using namespace::std;
 
   void Player::movesHelper(int index, std::vector<int> *validMoves, std::vector<char> *board){
     //keep going right until we get a value that isn't an opponents stone
-    int listOfDirections [] = {-7,-8,-9,-1,1,7,8,9};
+    int listOfDirections [] = {7,-9,-1,-8,8,-7,1,9};
     std::vector<int> directions(listOfDirections, listOfDirections + sizeof(listOfDirections) / sizeof(int) );
     for(int y = 0; y < directions.size(); y++){
       int x = 0; //x is our offset from our piece
       bool flag = false;
-      while(checkHelper(index + x, directions.at(y), board) == opposingColor)
+      try{
+         while(checkHelper(index + x, directions.at(y), board) == opposingColor)
       {
+        if((index+x)%8 == 7 && y > 4)
+        {
+          flag = false;
+          break;
+        }
+        else if((index+x)%8 == 0 && y < 3)
+        {
+          flag = false;
+          break;
+        }
+        else if(index+x+directions.at(y) > 63 || index+x+directions.at(y) < 0)
+        {
+          flag = false;
+          break;
+        }
         x += directions.at(y);
         flag = true;
       }
+      }catch(exception e){printf("TEST");fflush(stdout);}
+     
       x += directions.at(y);
       //if at least the first space is an opponents stone and the last one is empty
       //then we can make a move
-      if(checkHelper(index + x, directions.at(y), board)== '-' && flag){
+      if(flag){
         validMoves->push_back(index + x);
       }
     }
-    for(int i = 0; i < 64; i++)
+    for(int i = 0; i < board->size(); i++)
     {
       if(board->at(i) != '-')
       {
@@ -149,7 +167,7 @@ using namespace::std;
         {
           if(validMoves->at(j) == i)
           {
-            validMoves->erase(validMoves->begin()+j);
+            validMoves->at(j) = -1;
           }
         }
       }
@@ -172,6 +190,8 @@ using namespace::std;
   }
 int main(int argc, char* argv[]){
 
+  printf("LWLWLWL\n");
+  fflush(stdout);
   char* boardJSON = argv[2];
 
   char* boardStates = strtok(boardJSON, "[]");
@@ -190,15 +210,37 @@ int main(int argc, char* argv[]){
   char* color = argv[4];
   int time = atoi(argv[6]);
   Player* myPlayer = new Player(board, color[0], time);
+  printf("%s starting...",color);
+  fflush(stdout);
   vector<int>* validMoves = myPlayer->checkValidMoves(&board);
+  printf("FINISHED!!!\n");
+  fflush(stdout);
+  printf("Calculate Random Number...");
+  fflush(stdout);
   int randomNumber = rand() % validMoves->size();
+  printf("Finsihed!");
+  fflush(stdout);
   printf("Valid Moves:\n");
   for(int i = 0; i < validMoves->size(); i++)
   {
     printf("\t%i", validMoves->at(i));
   }
   printf("\n%s move: %i\n", color, validMoves->at(randomNumber));
-  int move = validMoves->at(randomNumber);
+  fflush(stdout);
+  int move = -1;
+  while(move < 0 || move > 63)
+  {
+    move = validMoves->at(randomNumber);
+    
+    randomNumber++;
+    randomNumber = randomNumber%validMoves->size();
+    if(move == -1)
+    {
+      printf("INVALID, trying again...\n");
+      printf("Current Size: %lu\n", validMoves->size());
+      printf("Next index to try: %i\n", randomNumber);
+    }
+  }
   delete validMoves;
   delete myPlayer;
   return move;
