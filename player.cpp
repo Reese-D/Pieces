36f489f39749f1a7c31babb879ac26ed55186d51
Reese-D <reese.dewind@gmail.com>
2015-10-24 13:30:02 -0400

    finished checkValidMoves
    finished check valid moves and fixed a few other problems

--- player.cpp
+++ player.cpp
@@ -31,57 +31,87 @@ using namespace::std;
       return false;
     return true;
   }
-  char Player::checkHelper(int index, int direction){
+  char Player::checkHelper(int index, int direction, std::vector<int> *board){
     try{
-      return board.at(index + direction);
+      return this->board.at(index + direction);
     }catch(std::out_of_range o){
       std::cout<<o.what()<<std::endl;
     }
     //returns an x if the value is out of bounds
     return 'x';
   }
-  char Player::checkAboveRight(int index){
+  char Player::checkAboveRight(int index,std::vector<int> *board){
     if(checkRightBounds(index))
-      return checkHelper(index, ABOVE + RIGHT);
+      return checkHelper(index, ABOVE + RIGHT, board);
     return 'x';
   }
-  char Player::checkAboveLeft(int index){
+  char Player::checkAboveLeft(int index,std::vector<int> *board){
     if(checkLeftBounds(index))
-      return checkHelper(index, ABOVE + LEFT);
+      return checkHelper(index, ABOVE + LEFT, board);
     return 'x';
   }
-  char Player::checkBelowLeft(int index){
+  char Player::checkBelowLeft(int index,std::vector<int> *board){
     if(checkLeftBounds(index))
-      return checkHelper(index, BELOW + LEFT);
+      return checkHelper(index, BELOW + LEFT, board);
     return 'x';
   }
-  char Player::checkBelowRight(int index){
+  char Player::checkBelowRight(int index,std::vector<int> *board){
     if(checkRightBounds(index))
-      return checkHelper(index, BELOW + RIGHT);
+      return checkHelper(index, BELOW + RIGHT, board);
     return 'x';
   }
-  char Player::checkAbove(int index){
-    return checkHelper(index, ABOVE);
+  char Player::checkAbove(int index,std::vector<int> *board){
+    return checkHelper(index, ABOVE, board);
   };
-  char Player::checkBelow(int index){
-    return checkHelper(index, BELOW);
+  char Player::checkBelow(int index,std::vector<int> *board){
+    return checkHelper(index, BELOW, board);
   }
-  char Player::checkLeft(int index){
+  char Player::checkLeft(int index,std::vector<int> *board){
     if(checkLeftBounds(index))
-      return checkHelper(index, LEFT);
+      return checkHelper(index, LEFT, board);
     return 'x';
   }
-  char Player::checkRight(int index){
+  char Player::checkRight(int index,std::vector<int> *board){
     if(checkRightBounds(index))
-      return checkHelper(index, RIGHT);
+      return checkHelper(index, RIGHT, board);
     return 'x';
   }
   void Player::scoreBoard(){}
-  std::vector<int> Player::checkValidMove(){
-    //TODO: finish function
-    std::vector<int> temp;
-    temp.reserve(6);
-    return temp;
+
+  /* takes a given board state and provides all possible valid moves
+  * ours: a vector of all of the indexes of our pieces on the board
+  * enemies: a vector of all the opposing players pieces on the board
+  */
+  std::vector<int> Player::checkValidMoves(std::vector<int> *board){
+    std::vector<int> *validMoves = new std::vector<int>();
+    //look through the board
+    for(int x = 0; x < this->board.size(); x++){
+      if(this->board.at(x) == color){
+        //if we find a piece of ours get all the valid moves around it
+        movesHelper(x, validMoves, board);
+      }
+      //TODO: check for repeats
+      //TODO: find the number of stones captured
+    }
+    return *validMoves;
+
+  }
+
+  void Player::movesHelper(int index, std::vector<int> *validMoves, std::vector<int> *board){
+
+    //keep going right until we get a value that isn't an opponents stone
+    int listOfDirections [] = {-7,-8,-9,-1,1,7,8,9};
+    std::vector<int> directions(listOfDirections, listOfDirections + sizeof(listOfDirections) / sizeof(int) );
+    for(int y = 0; y < directions.size(); y++){
+      int x = directions.at(y); //x is our offset from our piece
+      while(checkHelper(index + x, x, board) == opposingColor)
+        x += directions.at(y);
+      //if at least the first space is an opponents stone and the last one is empty
+      //then we can make a move
+      if(abs(x) > abs(directions.at(y)) && checkHelper(index + x, x, board)== 'x'){
+        validMoves->push_back(index + x);
+      }
+    }
   }
 
 int main(int argc, char* argv[]){
--- player.hpp
+++ player.hpp
@@ -9,18 +9,21 @@ public:
   char color;
   int time;
   Player(vector<char> in_board, char color, int time);
+  char opposingColor;
   bool checkRightBounds(int index);
   bool checkLeftBounds(int index);
-  char checkAboveRight(int index);
-  char checkAboveLeft(int index);
-  char checkBelowLeft(int index);
-  char checkBelowRight(int index);
-  char checkAbove(int index);
-  char checkBelow(int index);
-  char checkLeft(int index);
-  char checkRight(int index);
+  char checkAboveRight(int index, std::vector<int> *board);
+  char checkAboveLeft(int index, std::vector<int> *board);
+  char checkBelowLeft(int index, std::vector<int> *board);
+  char checkBelowRight(int index, std::vector<int> *board);
+  char checkAbove(int index, std::vector<int> *board);
+  char checkBelow(int index, std::vector<int> *board);
+  char checkLeft(int index, std::vector<int> *board);
+  char checkRight(int index, std::vector<int> *board);
   void scoreBoard();
-  std::vector<int> checkValidMove();
+  std::vector<int> checkValidMoves(std::vector<int> *board);
+  void movesHelper(int index, std::vector<int> *validMoves, std::vector<int> *board);
 private:
-  char checkHelper(int index, int direction);
+  char checkHelper(int index, int direction, std::vector<int> *board);
+
 };
